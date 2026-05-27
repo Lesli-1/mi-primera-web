b4de182ca1fb70f72a6b34d101e13034f9681fec
Lesli <leslimorales890@gmail.com>
2026-05-27 17:38:45 -0500

    Agrego JavaScript al proyecto

---
+++ script.js
@@ -0,0 +1,28 @@
+const subtitulo = document.getElementById("subtitulo")
+
+subtitulo.addEventListener("click", function() {
+  if (subtitulo.textContent === "Estudiante de desarrollo web") {
+    subtitulo.textContent = "¡Me hiciste clic!"
+  } else {
+    subtitulo.textContent = "Estudiante de desarrollo web"
+  }
+})
+
+const descripcion = document.getElementById("descripcion")
+
+descripcion.addEventListener("click", function() {
+  descripcion.style.color = "pink"
+})
+
+const campoNombre = document.getElementById("campo-nombre")
+const botonSaludar = document.getElementById("boton-saludar")
+const mensajeSaludo = document.getElementById("mensaje-saludo")
+
+botonSaludar.addEventListener("click", function() {
+  const nombre = campoNombre.value
+  if (nombre === "") {
+    mensajeSaludo.textContent = "¡Por favor escribe tu nombre!"
+  } else {
+  mensajeSaludo.textContent = "¡Hola, " + nombre + "!"
+}
+})
